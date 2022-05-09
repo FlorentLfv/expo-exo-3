d1a474c79d338191c55ec694c79e77d1eb4f28d5
Florent <florentlfv@gmail.com>
2022-05-09 16:06:38 +0200

    exercice 3

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.row}>
+        <View style={styles.greenContainer}></View>
+        <View style={styles.redContainer}></View>
+        <View style={styles.blueContainer}></View>
+      </View>
+      <View style={styles.column}>
+        <View style={styles.purpleContainer}></View>
+        <View style={styles.yellowContainer}></View>
+        <View style={styles.pinkContainer}></View>
+      </View>
     </View>
   );
 }
@@ -13,8 +20,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  row: {
+    flex: 1,
+    flexDirection: "row"
+  },
+  column: {
+    flex: 1,
+    flexDirection: "column"
+  },
+  greenContainer: {
+    flex: 3,
+    backgroundColor: 'green',
+  },
+  redContainer: {
+    flex: 2,
+    backgroundColor: 'red',
+  },
+  blueContainer: {
+    flex: 1,
+    backgroundColor: 'blue',
+  },
+  purpleContainer: {
+    flex: 3,
+    backgroundColor: 'purple',
+  },
+  yellowContainer: {
+    flex: 2,
+    backgroundColor: 'yellow',
+  },
+  pinkContainer: {
+    flex: 1,
+    backgroundColor: 'pink',
   },
 });
